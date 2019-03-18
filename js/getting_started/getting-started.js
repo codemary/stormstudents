9ad3d51083006778917296b49f07505cb95e6fd6
Hannah <hannahmariaklein@gmail.com>
2019-03-18 14:32:13 +0100

    finished 'getting started js'

---
+++ js/getting_started/getting-started.js
@@ -0,0 +1,15 @@
+console.log("This is an exercise in console logging.");
+
+var myValue = ("This is a Value.");
+console.log(myValue);
+
+var firstName = ("Hannah");
+var lastName = ("Klein");
+console.log(firstName, lastName);
+
+
+var myAge = ("29");
+var myCity = ("Berlin");
+var myJob = ("web developer");
+
+console.log("My name is " + firstName, lastName + ". I live in " + myCity + " and work as a " + myJob + ".");
