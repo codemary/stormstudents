9def1a2c4e852532bffef9709654a810f221a766
Hannah <hannahmariaklein@gmail.com>
2019-03-19 15:22:31 +0100

    finished data-types exercise

---
+++ js/data_typesP1/dataTypes.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+let myAnimal = "Fox - Vulpes vulpes";
+console.log(myAnimal);
+console.log(myAnimal[1], myAnimal[5]);
+
+let myName = "Hannah", myAge = 233, myYear = 2222;
+console.log(myName + " is " + myAge + " in " + myYear + ".");
+
+let isMarried = false;
+console.log(myName + " is married: " + isMarried);
+
+let programmingLanguage = "JavaScript", isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+console.log(programmingLanguage[0], programmingLanguage[4]);
+console.log(programmingLanguage.length);
