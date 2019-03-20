6eb0b9ddda3b39f50754930b1e72f74e257e2e69
Hannah <hannahmariaklein@gmail.com>
2019-03-20 11:59:33 +0100

    finished dataTypes2

---
+++ js/data_typesP2/dataTypes2.js
@@ -0,0 +1,42 @@
+let length = 16.78; // floating number
+let lastName = "Johnson"; // string
+let car = ""; // string
+let isOpen = true; // boolean
+console.log(typeof length);
+console.log(typeof lastName);
+console.log(typeof car);
+console.log(typeof isOpen);
+
+let ageMark = 39, ageJon = "39";
+console.log(ageMark);
+console.log(ageJon);
+
+let isOlder = false;
+console.log("Jon is older than Mark: " + isOlder);
+
+let x
+console.log(typeof x);
+
+x = "horizontal";
+console.log(typeof x);
+console.log(x[9]);
+console.log(x.length);
+console.log(typeof x.length);
+
+let a = 3, b = 4, c = 5;
+console.log(a==b);
+console.log(a==c);
+console.log(b==c);
+
+console.log(typeof a==b);
+
+/* infinity is a number */
+console.log(typeof Infinity);
+
+let num1 = 4;
+console.log(isNaN(num1));
+console.log("variable is NOT a number: " + isNaN(num1));
+/* console.log(typeof isNaN(num1)); */
+
+
+
