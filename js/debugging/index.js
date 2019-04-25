efc6ebcd110b62dc52ad9189c317e71a317fc7ef
Hannah <hannahmariaklein@gmail.com>
2019-04-25 11:58:24 +0200

    finished debugging index.js exercise

--- js/debugging/index.js
+++ js/debugging/index.js
@@ -1,15 +1,15 @@
-let fruits = ["orange", "apple", "strawberry", "lemon", "grapes";
+let fruits = ["orange", "apple", "strawberry", "lemon", "grapes"];
 console.log(fruits[0]);
 
-console.log(fruit.length);
+console.log(fruits.length);
 
 console.log(fruits.toString());
 
 console.log(typeof fruits);
 
-console.log(fruits.sort();
+console.log(fruits.sort());
 
-console.log(fruits[fruit.length - 1]);
+console.log(fruits[fruits.length - 1]);
 
 fruits.push("Lemon");
 console.log(fruits);
@@ -17,16 +17,16 @@ console.log(fruits);
 fruits[fruits.length] = "Melon";
 console.log(fruits);
 
-console.log(fruits.join" % ");
+console.log(fruits.join(" % "));
 
-let popValue = console.log(fruits.pop();
+let popValue = console.log(fruits.pop());
 console.log(fruits);
 
 let pushValue = fruits.push("fhdsjf");
 console.log(fruits);
 console.log(pushValue);
 
-console.log(fruits.shift);
+console.log(fruits.shift());
 console.log(fruits);
 
 fruits.unshift("Muskmelon");
@@ -35,8 +35,8 @@ console.log(fruits);
 fruits[0] = "mango";
 console.log(fruits);
 
-delete fruitsfruits.length-1;
-console.log(vegetable);
+delete fruits.length[-1];
+console.log(fruits);
 
 fruits.splice(2, 0, "peach", "Kiwi");
 console.log(fruits);
@@ -45,27 +45,27 @@ fruits.splice(2, 2, "peach", "Kiwi");
 console.log(fruits);
 
 fruits.splice(0, 1); 
-console.log(peach);
+console.log(fruits[2]);
 
 console.log('Hello World!');
 
 let varOne = 10;
 let varTwo = 10;
 
-if (((varOne >= varTwo) && (120 % 2 === 0) || (varOne * 260 === 2600)) {
+if (((varOne >= varTwo) && (120 % 2 === 0)) || (varOne * 260 === 2600)) {
     console.log("Heya!");
     (12 > 2) ? console.log("The condition is true") : console.log("the condition is false");
 
     let varThree;
     varThree = "1"; 
-    if (varThree == true {
+    if (varThree == true) {
         console.log("Heya is a string");
     }
 }
 
 let a = false;
 const b = "false";
-b = "true";
+//b = "true";
 
 if (a != b) {
     console.log("Booleans are not equal to strings!");
@@ -87,14 +87,14 @@ if (time < 10) {
 console.log(greeting);
 
 let myName = "Mariam 65465983795732489";
-console.log(myName[9, myName[20]);
-console.log(myNames.length);
+console.log(myName[9], myName[20]);
+console.log(myName.length);
 
-const currentYear = 2019;
+const currentYear = "2019";
 console.log(currentYear);
-console.log(currentYear[3);
+console.log(currentYear[3]);
 console.log(currentYear.length);
-currentYear = 2018;
+//currentYear = 2018;
 
 let myNumber = 1234e17;
 console.log(myNumber);
@@ -105,13 +105,14 @@ console.log(myNumber1);
 let myNumber2 = 1234e-5;
 console.log(myNumber2);
 
-console.log(newNum1 * -Infinity);
+console.log(myNumber1 * -Infinity);
 
 const country = "Germany";
-country = "France";
+//country = "France";
 console.log(country);
 
 let concat = "Hello";
+let concat2 = "World";
 let concat3 = concat + concat2;
 console.log(concat + " " + concat2 + "!");
 
@@ -122,6 +123,7 @@ let num4 = num1 + num2 + num3;
 console.log(num4);
 
 let num5 = "hello";
+let num6 = "world";
 let num7 = 20;
 let num8 = num5 + num6 + num7;
 console.log(num8);
@@ -129,11 +131,11 @@ console.log(num8);
 console.log(typeof dgfhdjfg7667hg);
 
 const isDay = true;
-isDay = false;
+//isDay = false;
 console.log(isDay);
 console.log(typeof isDay);
 
-console.log(Boolean(`$fgdgud`);
+console.log(Boolean(`$fgdgud`));
 
 let favColor = null;
 console.log(typeof favColor);
@@ -154,25 +156,25 @@ console.log(typeof myObject);
 
 let newFruitsArray = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 const citrus = newFruitsArray.slice(2);
-citrus = newFruitsArray[1];
+//citrus = newFruitsArray[1];
 console.log(citrus);
 console.log(newFruitsArray);
 
-let arrayToString = fruits.toString);
+let arrayToString = fruits.toString();
 console.log(arrayToString);
 console.log(typeof arrayToString);
-console.log(arrayToString[3); 
+console.log(arrayToString[3]); 
 
-let arrNum = [1, 2, 36, 345, true, 6645, false, "wajhdbfj", 8.6, null, "let", 9.0, -4875483;
-console.log(arrNum.sort);
-console.log(arrNum.reverse();
+let arrNum = [1, 2, 36, 345, true, 6645, false, "wajhdbfj", 8.6, null, "let", 9.0, -4875483];
+console.log(arrNum.sort());
+console.log(arrNum.reverse());
 
 console.log(fruits[fruits.length - 1]); 
 
 fruits.push("grapes");
 console.log(fruits);
 fruits.pop();
-console.log(fruits;
+console.log(fruits);
 fruits.pop;
 console.log(fruits);
 
@@ -184,10 +186,10 @@ fruits[0] = "Cherry";
 console.log(fruits);
 
 fruits[6] = "Grapes";
-console.log(fruity);
+console.log(fruits);
 
-delete fruitsfruits.length -1];
-console.log(fruit);
+delete fruits.length[-1];
+console.log(fruits);
 
 const javaScriptIsFun = true;
-javaScriptIsFun = false;
+//javaScriptIsFun = false;
