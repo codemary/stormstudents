1b0a76a0d11fdb000772255c4b263eb724111fd5
NextSnowflake <hannahmariaklein@gmail.com>
2019-04-29 23:03:49 +0200

    finished ternary exercise

--- js/ternary/ternary.js
+++ js/ternary/ternary.js
@@ -1,9 +1,18 @@
 //1. Declare a variable named canDrive. Check if someone's age is over 18. If so, print 'yes, can drive' and if not, print 'no, too young!'
+let age = 100;
+let canDrive = (age > 18) ? console.log("yes, can drive"):console.log("no, too young")
 
 //2. Declare a variable named speedCheck. If speed is above 130km, print 'you're going too fast!'. If speed is lower than 130km, print 'You're driving below the speed limit'.
+let speed = 111;
+let speedCheck = (speed > 130) ? console.log("you are going too fast."):console.log("You are going below the speed limit.")
 
 //3. Declare a variable named personAge. If age is below 21, print "serve juice". Otherwise print "serve wine".
+let personAge = (age < 21) ? console.log("serve juice."):console.log("serve wine.")
 
 //4. Declare a variable named isStudent. If true, print "Ticket costs €5,00". If false, print "Ticket costs €12,00".
+let student = false;
+let isStudent = (student === true) ? console.log("Ticket costs €5,00."):console.log("Ticket costs €12,00.")
 
 //5. Declare a variable named timeOfDay. Check if the time of day is 'morning' - if so, print "Good Morning". If not, print "Good Evening". 
+let timeOfDay = 20.00;
+let greeting = (timeOfDay < 12.00) ? console.log("good morning"):console.log("good evening");
