b3a4681641119c9dc40602e1fce9c47143baa189
Hannah <hannahmariaklein@gmail.com>
2019-06-03 14:22:27 +0200

    added "this"-keyword exercises in object1.js

--- js/objects/objects1.js
+++ js/objects/objects1.js
@@ -1,3 +1,4 @@
+console.log("-----1-----");
 /* 1. Write a program to list the properties of an object. 
 E.g.
 const student = { 
@@ -5,7 +6,6 @@ firstName: "John",
 lastName: "Smith", 
 class: 12 };
 Expected Output: firstName, lastName, class */
-
 let name_1 = "firstName", name_2 = "lastName", location = "class";
 
 const student1 = {
@@ -22,6 +22,24 @@ const student2 = {
 };
 console.log(student2);
 
+
+console.log("-----1a-----");
+
+function getStudentsData() {
+    let keys = Object.keys(this);
+    let values = Object.values(this);
+    console.log(keys, values)
+
+};
+
+getStudentsData.call(student1);
+
+
+
+
+
+
+console.log("-----2-----");
 //2. Create a person object. Include the person's first and last name, age, job, city etc. Then print text by retrieving data from the object e.g. "John Smith is a 41 year old engineer living in France".
 
 const personOfInterest = {
@@ -32,17 +50,54 @@ const personOfInterest = {
     country: "Russia",
     alias: "Black Widow"
 }
-console.log(`${personOfInterest[name_1]} ${personOfInterest[name_2]} is a ${personOfInterest.age} year old ${personOfInterest["job"]} from ${personOfInterest.country}, who goes under the alias ${personOfInterest.alias}.`)
+console.log(`${personOfInterest[name_1]} ${personOfInterest[name_2]} is a ${personOfInterest.age} year old ${personOfInterest["job"]} from ${personOfInterest.country}, who goes under the alias ${personOfInterest.alias}.`);
 
+
+console.log("-----2a-----");
+function getInfoText() {
+    console.log(`${Object.values(this)[0]} ${Object.values(this)[1]} is a ${Object.values(this)[2]} year old ${Object.values(this)[3]} from ${Object.values(this)[4]}, who goes under the alias ${Object.values(this)[5]}.`);
+};
+
+getInfoText.call(personOfInterest);
+
+
+
+console.log("-----3-----");
 //3. Write a JavaScript program to delete the "class" property (or last property) from the previous object.
-delete student2[location];
+// delete student2[location];
 console.log(student2);
 
+
+console.log("-----3a-----")
+
+function deleteLocation() {
+    delete this.class;
+    console.log(this);
+}
+deleteLocation.call(student2);
+
+
+
+
+console.log("-----4-----");
 //4. Write a program to get the length of a JavaScript object.
 console.log(Object.keys(personOfInterest).length);
 console.log(Object.keys(personOfInterest)[0]);
 
+console.log("-----4a-----");
 
+function getObjectLength() {
+    console.log(Object.keys(this).length);
+    console.log(Object.keys(this)[0]);
+}
+getObjectLength.call(personOfInterest);
+
+
+
+
+
+
+console.log("-----5-----");
 //5. Write a program to display the reading status (i.e. display book name, author name and reading status) of the following books.
 /* const library = [ 
    {
@@ -87,9 +142,20 @@ const library = [
         readingStatus: false
     }];
 
-console.log(library[0].readingStatus);
+//console.log(library[0].readingStatus);
  for (let i = 0; i < library.length; i++) {
     if (library[i].readingStatus === true) {
         console.log(`Already read '${library[i].title}' by ${library[i].author}`)
     } else console.log(`You still need to read '${library[i].title}' by ${library[i].author}`);
 };
+
+console.log("-----4a-----");
+
+function getReadingStatus() {
+    for (let i = 0; i < this.length; i++) {
+        if (this[i].readingStatus === true) {
+            console.log(`Already read '${this[i].title}' by ${this[i].author}`)
+        } else console.log(`You still need to read '${this[i].title}' by ${this[i].author}`);
+    };
+}
+getReadingStatus.call(library);
