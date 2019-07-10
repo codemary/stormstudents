485e921014b883b547e4ca2d541ebf1e15b81266
Hannah <hannahmariaklein@gmail.com>
2019-07-10 11:49:23 +0200

    added css style loader to webpack prototype

--- 1-JavaScript-Classwork/z-DOM/4-Modules/Webpack/webpack-prototype/src/index.js
+++ 1-JavaScript-Classwork/z-DOM/4-Modules/Webpack/webpack-prototype/src/index.js
@@ -1,7 +1,13 @@
+import createBanner from "./banner.js";
+
 const createElement = message => {
-    const element = document.createElement("div");
-    element.innerHTML = message;
-    return element;
-  };
-  
-  document.body.appendChild(createElement("Webpack lives by the love of Open Source."));
+  const element = document.createElement("div");
+  element.innerHTML = message;
+  return element;
+};
+
+document.body.appendChild(
+  createElement("Webpack lives by the love of Open Source.")
+);
+
+document.body.appendChild(createBanner());
